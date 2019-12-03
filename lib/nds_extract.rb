af785ef3d86c45fd9b17050531fc2ad28247c0c9
Daniel Liu <danliu277@gmail.com>
2019-12-03 19:21:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,10 +7,31 @@ require 'directors_database'
 
 def directors_totals(nds)
   result = {}
-  nil
+  index1 = 0
+  index2 = 0
+  # pp nds[0]
+  
+  while nds[index1] do
+    sum = 0
+    while nds[index1][:movies][index2] do
+      sum += nds[index1][:movies][index2][:worldwide_gross]
+      index2 += 1
+    end
+    index1 += 1
+    index2 = 0
+    result.merge!(Hash[nds[index1][:name], sum])
+  end
+  return result
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  sum = 0
+  index = 0
+  while director_data[:movies][index] do
+    sum += director_data[:movies][index][:worldwide_gross]
+    index += 1
+  end
+  return sum
 end
